8a7ea9962e9f7c083cf4e50cac9838a4d89fc461
EamonLmao3.5 <eamon.liew@online.island.edu.hk>
2022-01-11 08:52:48 +0800

    Initial commit

---
+++ Topics/Integer arithmetic (1)/A simple expression/main.py
@@ -0,0 +1,8 @@
+# work with these variables
+a = int(input())
+b = int(input())
+c = int(input())
+d =(a * b)
+e = (d - c)
+print(e)
+
---
+++ Topics/Program with numbers/Calculating an expression/main.py
@@ -0,0 +1,3 @@
+n = int(input())
+k = ((n + 1) * n + 2) * n + 3
+print(k)
